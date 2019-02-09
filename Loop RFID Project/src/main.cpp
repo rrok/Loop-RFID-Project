33046963a768ced7975f030be064976dd7b6eb0d
rrok <rrok@visitor-44631938.dyndns.cern.ch>
2019-02-09 11:33:45 +0100

    start

--- Loop RFID Project/src/main.cpp
+++ Loop RFID Project/src/main.cpp
@@ -6,6 +6,7 @@ void setup() {
   //efrecon/mqtt-client pub -h localhost -t "mytest" -m "ok"
   Homie_setFirmware("bare-minimum", "1.0.0"); // The underscore is not a typo! See Magic bytes
   Homie.setup();
+  //ready
 }
 
 void loop() {
