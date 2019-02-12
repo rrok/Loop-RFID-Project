8e1aea87d6bc401195cc7e141aaf10443e5b6060
rrok <rro.gjinaj@gmail.com>
2019-02-12 11:11:33 +0100

    testing

--- Loop RFID Project/src/main.cpp
+++ Loop RFID Project/src/main.cpp
@@ -1,40 +1,49 @@
 #include <Homie.h>
 #include "MFRC522.h"
-const int TEMPERATURE_INTERVAL = 300;
-unsigned long lastTemperatureSent = 0;
-HomieNode temperatureNode("temperature", "temperature");
-
-
 String uid;
-#define RST_PIN  9  // RST-PIN für RC522 - RFID - SPI - Modul GPIO5 
+String data="";
+#define RST_PIN  8  // RST-PIN für RC522 - RFID - SPI - Modul GPIO5 
 #define SS_PIN  4  // SDA-PIN für RC522 - RFID - SPI - Modul GPIO4 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
-HomieNode rfidNode("RFID-1", "sensor");
+HomieNode rfidNode("READER", "CPR");
 unsigned long previousMillis = 0;
 const int interval = 2000;
+StaticJsonBuffer<300> JSONbuffer;
+JsonObject& JSONencoder = JSONbuffer.createObject();
 
 void setupHandler() {
   Serial.begin(9600);    // Initialize serial communications
   SPI.begin();           // Init SPI bus
   mfrc522.PCD_Init();    // Init MFRC522
-  temperatureNode.setProperty("unit").send("c");
-  rfidNode.setProperty("uid").send("id");
 }
 // Global Timer
 
 void dump_byte_array(byte *buffer, byte bufferSize) {
   String uid;
-  const long interval = 1000;
-
   for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
+    //Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    //Serial.print(buffer[i], HEX);
+    
     uid = uid + String(buffer[i], HEX);
   
   }
     if(uid){
-     //6Homie.setProperty(rfidNode, "uid", uid, true);
-     rfidNode.setProperty(uid).send(uid);
+      Homie.getLogger()<<"uid Found"<< uid <<endl;
+      JSONencoder["sensorType"] = "READER RFID";
+      JSONencoder["companyID"] = "CEDI-01";
+      JSONencoder["companyName"] = "Commercianti Indipendenti Associati Soc. Coop.";
+      JSONencoder["address"] = "Via dei Mercanti, 3, 47122 Forlì FC, Italia";
+      JSONencoder["temperature"] = "23";
+      JSONencoder["lat"] = "44.2347418";
+      JSONencoder["lon"] = "12.0612333";
+      JSONencoder["boxUID"] = uid;
+      char JSONmessageBuffer[300];
+      JSONencoder.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+      //rfidNode.setProperty(uid).send(uid);
+      Serial.print(F("\nCard UID:"));
+      Serial.print(uid);
+      rfidNode.setProperty("data").send("JSONmessageBuffer");
+      
   }
 }
 
@@ -45,7 +54,7 @@ void loopHandler() {
     delay(50);
     return;
   }
-
+  Homie.getLogger()<<"Card Present" <<endl;
   // scan the cards. Put in a non-blocking delay to avoid duplicate readings
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
@@ -54,13 +63,15 @@ void loopHandler() {
 
     // Select one of the cards
     if ( ! mfrc522.PICC_ReadCardSerial()) {
-      Serial.print("found card...");
+        Homie.getLogger()<<"reading" <<endl;
+      //Serial.print("found card...");
       delay(50);
       return;
     }
 
     // Process card
-    Serial.print(F("Card UID:"));
+    //Serial.print(F("\nCard UID:"));
+    Homie.getLogger()<<"card information" <<endl;
     dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
     }
 }
@@ -68,8 +79,9 @@ void loopHandler() {
 void setup() {
   Serial.begin(9600);
   Serial << endl << endl;
-  Homie_setFirmware("rfid", "1.0.0");
+  Homie_setFirmware("READER", "1.0.0");
   Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
+  Homie.setLoggingPrinter(&Serial);
   Homie.setup();
 
 }
